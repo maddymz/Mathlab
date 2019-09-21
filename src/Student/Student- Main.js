8efda7740e71616a927efc5792cd082b7e5b5b3c
Sajith Thattazhi <sajiththattazhi14@gmail.com>
2019-09-20 23:55:16 -0700

    Added Student-Main page

---
+++ src/Student/Student- Main.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+class StudentMain extends Component {
+	//constructor(props){
+	//	super(props);
+	//	this.state={
+	//		username:'',
+	//		password:''
+	//	}
+	//}
+	
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+const style = {
+	margin: 15,
+};
+
+export default StudentMain;
