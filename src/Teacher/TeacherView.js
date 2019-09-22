ce67174b706e1a0898d32b81005ff320a60c07e4
Bhavana <bhavanavakkalagadda@gmail.com>
2019-09-22 14:01:19 -0700

    alignment and final version of this userstory

--- src/Teacher/TeacherView.js
+++ src/Teacher/TeacherView.js
@@ -7,6 +7,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 class Teacher extends Component {
 
     render() {
+        const Customstyle = {float : "left"}
+        const rightstyle = {float : "right"}
     return (
         <div>
             <MuiThemeProvider>
@@ -15,13 +17,17 @@ class Teacher extends Component {
                         title="Teacher"
                         color="Black"
                     />
+                    <RaisedButton label="LogOut" primary={true} style={rightstyle} onClick={(event) => this.handleClick(event)}/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
                     <p>Want to Create Quiz?</p>
                     <br/>
                     <RaisedButton label="Create Quiz" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-                    <br/>
-                    <p>Want to LogOut?</p>
-                    <br/>
-                    <RaisedButton label="LogOut" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
                     <br/>
                 </div>
             </MuiThemeProvider>
