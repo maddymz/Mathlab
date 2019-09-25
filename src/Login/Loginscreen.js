e2067e81829f178ced37201be62fda7a1b1b7a59
Viraj Khatri <virajkhatri41@gmail.com>
2019-09-25 16:03:16 -0700

    Username and Password validation with action of submit button

--- src/Login/Loginscreen.js
+++ src/Login/Loginscreen.js
@@ -11,6 +11,23 @@ constructor(props){
   password:''
   }
  }
+
+ handleClick() {
+  var username = this.state.username
+  var password = this.state.password
+  var usernameRegex = /^[a-zA-Z ]{2,30}$/;
+  var passwordRegex = /^[a-zA-Z1-9]{8,20}$/;
+  if (usernameRegex.test(username)) {
+    if (passwordRegex.test(password)) {
+      alert("Welcome " + username)
+    } else {
+      alert("Issue with password. Length should be between 8-30 containing alphabets and numbers.")
+    }
+  } else {
+    alert("Issue with username. Length should be between 2-30 containing just alphabets")
+  }
+}
+
 render() {
     return (
       <div>
