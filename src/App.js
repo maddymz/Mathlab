b96be359284eb6664eacaf7860e0a2175dfad3df
Bhavana <bhavanavakkalagadda@gmail.com>
2019-10-15 18:11:34 -0700

    Task 65 US 33 Implemented routing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import StudentMain from './Components/Student/StudentMain';
 import StudentPractice from './Components/Student/StudentPractice';
 import StudentQuiz from './Components/Student/StudentQuiz';
 import TeacherView from './Components/Teacher/TeacherView'
-
+import CreateQuiz from './Components/Teacher/CreateQuiz'
+import StartQuiz from './Components/Teacher/StartQuiz';
 class App extends Component {
   render() {
     return (
@@ -17,6 +18,8 @@ class App extends Component {
             <Route path="/student/practice" component={StudentPractice} />
             <Route path="/student/quiz" component={StudentQuiz} />
             <Route path="/teacher" component={TeacherView} />
+            <Route path='/quiz' component={CreateQuiz}/>
+            <Route path='/teacher/startquiz' component={StartQuiz}/>
           </Switch>
         </div>
       </BrowserRouter>
