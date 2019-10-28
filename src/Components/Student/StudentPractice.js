40f0f929f801372dc58317c493a35fd71db3a32b
maddymz <madhukar4u.rm@gmail.com>
2019-10-27 23:51:41 -0700

    Revert "imeplemented US#2 Task#89"
    This reverts commit 311d0c85b2bbcaf69d151c6b4ddbb8922179d172.
    
    reverting the changes to recommit as pushed chnages directly to
    development branch

--- src/Components/Student/StudentPractice.js
+++ src/Components/Student/StudentPractice.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import { DndProvider} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import './Student.css';
 import Header from '../Header/Header';
-import DragnDrop from '../DragnDrop/dragndrop';
 
 class StudentPractice extends Component {
 	constructor(props) {
@@ -23,7 +19,6 @@ class StudentPractice extends Component {
 		this.props.history.push('/student', this.state)
 	}
 
-	
 	render() {
 		return (
 			<div className="StudentPractice">
@@ -33,14 +28,8 @@ class StudentPractice extends Component {
 					</div>
 					<div>
 						<Paper style={style}>
-						<div className="drag-n-drop">
-							<DndProvider backend={HTML5Backend}>
-								<DragnDrop/>
-							</DndProvider>
-						</div>
+							This is where the practice section goes.
 						</Paper>
-
-						
 					</div>
 					<div>
 						<RaisedButton label="Back" primary={true} style={style} onClick={(event) => this.goBackToStudent()} />
